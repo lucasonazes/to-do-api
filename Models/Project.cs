9b9db9a18a859dc424162d71c4854a0320cadd9e
Thiago Tinfre Marcelino <thiagotinfre@hotmail.com>
2024-10-20 16:04:21 -0300

    Criação dos CRUDs e Rotas

---
+++ Models/Project.cs
@@ -0,0 +1,10 @@
+namespace api.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime FinalDate { get; set; }
+}
